1ee9cff4ded9a39ee4e05eb36714fc6890d4fa53
ASHUZA <ashuzakasumbepascal19@gmail.com>
2021-09-01 16:50:10 -0100

    add gets services for mode_arrivee

--- src/mode-arrivee/mode-arrivee.service.ts
+++ src/mode-arrivee/mode-arrivee.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ModeArriveeEntity } from './entities/mode-arrivee.entity';
 
 @Injectable()
-export class ModeArriveeService {}
+export class ModeArriveeService {
+    constructor(
+        @InjectRepository(ModeArriveeEntity)
+        private ModeArriveeRepository: Repository<ModeArriveeEntity>,
+    ){}
+
+    async findModeArriveeById(id: number){
+        const mode_arrivee = await this.ModeArriveeRepository.findOne(id);
+        if (!mode_arrivee){
+            throw new NotFoundException(`Le mode d'arrivee d'id ${id} n'existe pas.`);
+        }
+        return mode_arrivee;
+    }
+    async getMode_arrivees(): Promise<ModeArriveeEntity[]>{
+        return await this.ModeArriveeRepository.find();
+    }
+async getMode_arroiveeById(id: number): Promise<ModeArriveeEntity>{
+    return await this.findModeArriveeById(id);
+}
+ 
+}
