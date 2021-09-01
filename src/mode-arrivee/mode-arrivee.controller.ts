06e6f756dd8197438c7cda4e303ca2c73e7a4cd5
ASHUZA <ashuzakasumbepascal19@gmail.com>
2021-09-01 18:52:27 -0100

    add post , update and remove for mode_arrivee controller

--- src/mode-arrivee/mode-arrivee.controller.ts
+++ src/mode-arrivee/mode-arrivee.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, ParseIntPipe, Param } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Param, Patch, Delete, Body, Post } from '@nestjs/common';
+import { NewModeArriveeDto } from './dto/new_mode_arrivee_dto';
 import { ModeArriveeEntity } from './entities/mode-arrivee.entity';
 import { ModeArriveeService } from './mode-arrivee.service';
 
@@ -13,5 +14,27 @@ export class ModeArriveeController {
     @Get('/:id')
     async getModeArriveeById(@Param('id', ParseIntPipe) id: number){
         return await this.ModeArriveeService.getMode_arroiveeById(id);
+    }
+    @Post()
+    async addModeArrivee(
+        @Body() ModeArrivee: NewModeArriveeDto,
+    ): Promise<ModeArriveeEntity> {
+        return await this.ModeArriveeService.addModeArrivee(ModeArrivee);
+    }
+    @Patch('/:id')
+    async updateModeArrivee(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() ModeArrivee: NewModeArriveeDto,
+    ): Promise<ModeArriveeEntity> {
+        return await this.ModeArriveeService.updateModeArrivee(
+            id,
+            ModeArrivee,
+        );
+    }
+    @Delete('/:id')
+    async deleteModeArrivee(
+        @Param('id', ParseIntPipe) id: number,
+    ): Promise<ModeArriveeEntity> {
+        return await this.ModeArriveeService.deleteModeArrivee(id);
     }
 }
--- src/mode-arrivee/mode-arrivee.service.ts
+++ src/mode-arrivee/mode-arrivee.service.ts
@@ -1,27 +1,50 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { NewModeArriveeDto } from './dto/new_mode_arrivee_dto';
 import { ModeArriveeEntity } from './entities/mode-arrivee.entity';
 
 @Injectable()
 export class ModeArriveeService {
     constructor(
         @InjectRepository(ModeArriveeEntity)
-        private ModeArriveeRepository: Repository<ModeArriveeEntity>,
+        private modeArriveeRepository: Repository<ModeArriveeEntity>,
     ){}
 
     async findModeArriveeById(id: number){
-        const mode_arrivee = await this.ModeArriveeRepository.findOne(id);
+        const mode_arrivee = await this.modeArriveeRepository.findOne(id);
         if (!mode_arrivee){
             throw new NotFoundException(`Le mode d'arrivee d'id ${id} n'existe pas.`);
         }
         return mode_arrivee;
     }
     async getMode_arrivees(): Promise<ModeArriveeEntity[]>{
-        return await this.ModeArriveeRepository.find();
+        return await this.modeArriveeRepository.find();
     }
 async getMode_arroiveeById(id: number): Promise<ModeArriveeEntity>{
     return await this.findModeArriveeById(id);
 }
- 
+async addModeArrivee(
+    ModeArrivee: NewModeArriveeDto,
+): Promise<ModeArriveeEntity>{
+    return await this.modeArriveeRepository.save(ModeArrivee);
+}
+
+async updateModeArrivee(
+    id: number,
+    ModeArrivee: NewModeArriveeDto,
+): Promise<ModeArriveeEntity>{
+    const newModeArrivee = await this.modeArriveeRepository.preload({
+        id,
+        ...ModeArrivee,
+    });
+    if (!newModeArrivee){
+        throw new NotFoundException(`Le mode d'arrivée d'id ${id} n'existe pas.`);
+    }
+    return await this.modeArriveeRepository.save(newModeArrivee);
+}
+async deleteModeArrivee(id: number):Promise<ModeArriveeEntity> {
+    const ModeArrivee = await this.findModeArriveeById(id);
+    return await this.modeArriveeRepository.remove(ModeArrivee)
+}
 }
