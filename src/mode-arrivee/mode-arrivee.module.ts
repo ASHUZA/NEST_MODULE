c71deef8d0cf04f2cf78da2e2ce97e2b53bb840e
ASHUZA <ashuzakasumbepascal19@gmail.com>
2021-09-01 16:26:29 -0100

    import mode_arrivee entity to mode_arrivee module

--- src/mode-arrivee/mode-arrivee.module.ts
+++ src/mode-arrivee/mode-arrivee.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ModeArriveeEntity } from './entities/mode-arrivee.entity';
 import { ModeArriveeController } from './mode-arrivee.controller';
 import { ModeArriveeService } from './mode-arrivee.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ModeArriveeEntity])],
   controllers: [ModeArriveeController],
   providers: [ModeArriveeService]
 })
