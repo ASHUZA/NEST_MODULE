0ab6d5385047584966dc81756f20c70ca7ae80b4
ASHUZA <ashuzakasumbepascal19@gmail.com>
2021-09-01 17:02:32 -0100

    add gets controllers for mode_arrivee

--- src/mode-arrivee/mode-arrivee.controller.ts
+++ src/mode-arrivee/mode-arrivee.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Param } from '@nestjs/common';
+import { ModeArriveeEntity } from './entities/mode-arrivee.entity';
+import { ModeArriveeService } from './mode-arrivee.service';
 
 @Controller('mode-arrivee')
-export class ModeArriveeController {}
+export class ModeArriveeController {
+    constructor(private ModeArriveeService: ModeArriveeService) {}
+
+    @Get()
+    async getAllModeArrivees(): Promise<ModeArriveeEntity[]>{
+        return await this.ModeArriveeService.getMode_arrivees();
+    }
+    @Get('/:id')
+    async getModeArriveeById(@Param('id', ParseIntPipe) id: number){
+        return await this.ModeArriveeService.getMode_arroiveeById(id);
+    }
+}
